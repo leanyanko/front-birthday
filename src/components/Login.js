c2c426bc547240b48e5f6cdb5753e986c134b4c9
meinou <mskozlovskaya@gmail.com>
2018-07-07 17:40:05 -0400

    html fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,13 @@ import "./Login.css";
 
 class Login extends Component {
   render() {
-    return <div className="Login"><img src="birthday.jpg" alt="birthday">
-    <h1>This is a Heading</h1>
-    <p>This is a paragraph.</p>
-    </div>;
+    return (
+      <div className="Login">
+        <img src="birthday.jpg" alt="birthday" />
+        <h1>This is a Heading</h1>
+        <p>This is a paragraph.</p>
+      </div>
+    );
   }
 }
 
