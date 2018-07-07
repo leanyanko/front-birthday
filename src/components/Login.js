056c30c75133526d42a54f2351660864c6493d17
Chirihan <chirihan.chergui@groupe-esigelec.org>
2018-07-07 17:26:29 -0400

    vfve

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Login.css";
+
+class Login extends Component {
+  render() {
+    return <div className="Login"><img src="birthday.jpg" alt="birthday">
+    <h1>This is a Heading</h1>
+    <p>This is a paragraph.</p>
+    </div>;
+  }
+}
+
+export default Login;
