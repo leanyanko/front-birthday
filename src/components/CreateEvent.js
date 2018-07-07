1be94ebd594f644a5341ca1f5792c09bb2aa8d79
meinou <mskozlovskaya@gmail.com>
2018-07-07 15:41:06 -0400

    event component

---
+++ src/components/CreateEvent.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./CreateEvent.css";
+
+class CreateEvent extends Component {
+  render() {
+    return <div className="createevent">HEADER</div>;
+  }
+}
+
+export default CreateEvent;
